ff9fca756b64b7d8fa981b0929f54e37983b70c0
Nicolas LIENART <nicolas@lienart.net>
2019-05-22 16:28:52 +0200

    Animation de Sprite

---
+++ src/Sprite/animation.h
@@ -0,0 +1,19 @@
+#ifndef ANIMATION_H_
+#define ANIMATION_H_
+
+#include <GL/gl.h>
+#include <stdlib.h>
+#include <SDL2/SDL_image.h>
+
+#include "structures.h"
+
+
+// NOTE: Un peu à part
+SpriteImage *animation_loadSprite(char image_path[], int total_splitX, int total_splitY, Uint32 loop_duration);
+
+SpriteTexture* animation_loadTexture(SpriteImage *sprite_data);
+void animation_displaySprite(SpriteTexture* sprite);
+void animation_updateSprite(SpriteTexture* sprite);
+void animation_translateTexture(SpriteTexture* sprite);
+
+#endif //ANIMATION_H_
--- src/Sprite/structures.h
+++ src/Sprite/structures.h
@@ -1,4 +1,25 @@
 #ifndef STRUCTURES_H_
 #define STRUCTURES_H_
+
+#include <GL/gl.h>
+#include <SDL2/SDL.h>
+
+typedef struct SpriteImage {
+  SDL_Surface* surface;
+  Uint32 loop_duration;
+  int sprite_totalX;
+  int sprite_totalY;
+} SpriteImage;
+
+typedef struct SpriteTexture {
+  int sprite_totalX;
+  int sprite_totalY;
+  int sprite_numX;
+  int sprite_numY;
+  Uint32 loop_duration;
+  Uint32 last_frame;
+  GLuint texture_id;
+} SpriteTexture;
+
 #endif //STRUCTURES_H_
 
